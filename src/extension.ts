3819a6b1cf1fcd5624f05f3553f7f42cfd01a19a
Adam Hartford <adam@adamhartford.com>
2016-02-17 13:23:19 -0500

    Removed template comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 import Window = vscode.window;
 import Range = vscode.Range;
@@ -8,17 +7,10 @@ import Selection = vscode.Selection;
 import TextDocument = vscode.TextDocument;
 import TextEditor = vscode.TextEditor;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-base64" is now active!');
+	console.log('vscode-base64" is now active');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let encode = vscode.commands.registerCommand('extension.base64Encode', () => {    
         let e = Window.activeTextEditor;
 		let d = e.document;
@@ -56,6 +48,5 @@ function base64Decode(e: TextEditor, d: TextDocument, sel: Selection[]) {
 	}
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
